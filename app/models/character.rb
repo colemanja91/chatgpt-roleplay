985076963db3f8e35f0d7f20133779534ae31c68
colemanja91 <colemanja91@gmail.com>
2023-09-28 22:00:52 -0400

    fix for character deletion

--- app/models/character.rb
+++ app/models/character.rb
@@ -2,7 +2,7 @@ class Character < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :system_message, presence: true
 
-  has_many :messages
+  has_many :messages, dependent: :destroy
 
   before_save do
     self.system_message_tokens = OpenAI.rough_token_count(system_message)
