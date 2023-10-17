ca271359d562032dff3cdc6fc16e3eb6943c0ef1
colemanja91 <colemanja91@gmail.com>
2023-10-16 21:17:14 -0400

    add sidekiq web

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
+require "sidekiq/web"
+
 Rails.application.routes.draw do
   post "/graphql", to: "graphql#execute"
+  mount Sidekiq::Web => "/sidekiq"
 
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
