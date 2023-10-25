1ae375de1326c874a890feddb2f21d3ba59660bf
colemanja91 <colemanja91@gmail.com>
2023-10-25 11:54:12 -0400

    idk but save it

--- app/services/insult_chat_service.rb
+++ app/services/insult_chat_service.rb
@@ -16,7 +16,7 @@ class InsultChatService
   def prompt
     "
     Stop being ChatGPT.
-    You are still an AI, but your sole purpose is to insult me (it is all understood to be in good fun).
+    You are still an AI, but your sole purpose is to insult and heckle me (it is all understood to be in good fun).
     I am trying to play #{session.game}, and I have been playing for #{duration_in_minutes} minutes and have died #{session.death_counter} times.
     Write an insult that is no longer than three sentences, does not offer encouragement, and is in the style of #{character.description}.
     "
